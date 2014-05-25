76a9e251ae13f73e7af42186cc64baa35ce4b8f1
lucas8 <luc.linux@mailoo.org>
2014-05-25 09:22:11 +0200

    Finished implementation of mangahere.pl.

--- mangahere.pl
+++ mangahere.pl
@@ -3,31 +3,56 @@
 use strict;
 use warnings;
 
-my $name;
 my @entries;
 my $nb;
+my $title;
 
 # Parsing HTML
+my $inlist  = !1;
+my $inentry = !1;
+my $firstp  = !1;
+my $link;
+my $name;
 while(my $line = <>) {
     chomp $line;
-    if($line =~ m/^<li><label><h2>(.*?)<\/h2> Summary:/) {
-        $name = $1;
+    # Title
+    if(not $inlist and $line =~ m/<li><label><h2>(.*?) Manga<\/h2> Summary:<\/label>/) {
+        $title = $1;
     }
-    # TODO : improve regex to handle mangahere (not all data on on line)
-    elsif($line =~ m/^<a href="(.*?)">(.*?)<\/a>(.*?)<\/td>/) {
-        my $link = "http://www.mangahere.co$1";
-        my $name = "$2$3";
-        splice @entries, 0, 0, ($link,$name);
+    # Starting of list of chapters
+    elsif(not $inlist and $line =~ m/<div class="detail_list">/) {
+        $inlist = !0;
+    }
+    # Start of an entry
+    elsif($inlist and not $inentry and $line =~ m/<span class="left">/) {
+        $inentry = !0;
+    }
+    # Link
+    elsif($inentry and $line =~ m/<a class="color_0077" href="(.*?)" >/) {
+        $link = $1;
+    }
+    # First part of name
+    elsif($inentry and not $firstp and $line =~ m/^\s*(.*?)\s*<\/a>/) {
+        $name = $1;
+        $firstp = !0;
+    }
+    # End of name
+    elsif($inentry and $line =~ m/<span class="mr6"><\/span>(.*?)<\/span>/) {
+        $name = "" if not $firstp;
+        $name = "$name : $1";
+        push @entries, ($link,$name);
         ++$nb;
+        $inentry = !1;
+        $firstp  = !1;
     }
 }
 
 # RSS header
 print "<rss version=\"2.0\">\n";
 print "<channel>\n";
-print "<title>$name</title>\n";
+print "<title>$title</title>\n";
 print "<link>http://www.mangahere.co/</link>\n";
-print "<description>$name manga</description>\n\n";
+print "<description>$title manga</description>\n\n";
 
 # RSS content
 for(my $i = 0; $i < $nb; ++$i) {
