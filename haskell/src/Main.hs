655f4de60437c4696204918617c6ee56535ca5d2
lucas8 <luc.linux@mailoo.org>
2018-03-30 07:08:53 +0200

    Add infrastructure

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,7 +1,38 @@
 
 module Main where
+
+import Text.XML.HXT.Core
+import Text.XML.HXT.Arrow.XmlState.RunIOStateArrow
+import System.Environment
 import RSS
 
+type Scrapper = ( IOSLA (XIOState ()) XmlTree RSSFeed
+                , IOSLA (XIOState ()) XmlTree RSSItem
+                )
+
+runScrapper :: Scrapper -> IOSLA (XIOState ()) () ()
+runScrapper (afeed, aitem) =
+     readDocument [ withValidate no
+                  , withParseHTML yes
+                  ]
+                  ""
+ >>> generateFeed afeed aitem
+ >>> writeDocument [] ""
+ >>^ const ()
+
+scrappers :: [(String, Scrapper)]
+scrappers = []
+
+run :: String -> IO ()
+run scrap = case lookup scrap scrappers of
+              Nothing -> return ()
+              Just sc -> runIOSLA (runScrapper sc) (initialState ()) ()
+                      >> return ()
+
 main :: IO ()
-main = putStrLn "Hello World !"
+main = do
+    ags <- getArgs
+    case ags of
+      [scrap] -> run scrap
+      _       -> putStrLn "usage: program scrapper_name"
 
--- haskell/src/RSS.hs
+++ haskell/src/RSS.hs
@@ -25,14 +25,15 @@ generateItem =
         , selem "description" [ arr item_description >>> mkText ]
         ]
 
-generateFeed :: ArrowXml a => RSSFeed -> a b RSSItem -> a b XmlTree
-generateFeed (RSSFeed title link description) item_arrow =
-    mkelem "rss" [ sattr "version"    "2.0"
-                 , sattr "xmlns:atom" "http://www.w3.org/2005/Atom"
-                 ]
-        [ selem "title"       [ txt title       ]
-        , selem "link"        [ txt link        ]
-        , selem "description" [ txt description ]
-        , item_arrow >>> generateItem
-        ]
+generateFeed :: ArrowXml a => a b RSSFeed -> a b RSSItem -> a b XmlTree
+generateFeed feed_arrow item_arrow =
+     (feed_arrow &&& item_arrow)
+ >>> mkelem "rss" [ sattr "version"    "2.0"
+                  , sattr "xmlns:atom" "http://www.w3.org/2005/Atom"
+                  ]
+         [ selem "title"       [ arr fst >>> arr feed_title       >>> mkText ]
+         , selem "link"        [ arr fst >>> arr feed_link        >>> mkText ]
+         , selem "description" [ arr fst >>> arr feed_description >>> mkText ]
+         , arr snd >>> generateItem
+         ]
 
